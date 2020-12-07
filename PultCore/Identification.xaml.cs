94fd1fd4a4b00c0c4b0e55e4158d17d54a097f35
elzantursunbaev@gmail.com <elzantursunbaev@gmail.com>
2020-12-07 13:44:16 +0600

    Добавьте файлы проекта.

---
+++ PultCore/Identification.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PultCore
+{
+    /// <summary>
+    /// Логика взаимодействия для Identification.xaml
+    /// </summary>
+    public partial class Identification : Window
+    {
+        public Identification()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            window.Show();
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TextBox_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Login.Text == "Логин")
+            {
+                Login.Text = "";
+            }
+        }
+
+        private void Login_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Login.Text == "")
+            {
+                Login.Text = "Логин";
+            }
+        }
+
+        private void Password_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Password.Text == "")
+            {
+                Password.Text = "Пароль";
+            }
+        }
+
+        private void Password_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Password.Text == "Пароль")
+            {
+                Password.Text = "";
+            }
+        }
+    }
+}
---
+++ PultCore/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PultCore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var desctop = System.Windows.SystemParameters.WorkArea;
+            this.Left = desctop.Right - (this.Width);
+            this.Top = desctop.Bottom - (this.Height);
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var desctop = System.Windows.SystemParameters.WorkArea;
+            this.Left = desctop.Right - (this.Width - 300);
+            this.Top = desctop.Bottom - this.Height;
+        }
+
+        private void Grid_MouseLeave(object sender, MouseEventArgs e)
+        {
+            var desctop = System.Windows.SystemParameters.WorkArea;
+            this.Left = desctop.Right - (this.Width - 300);
+            this.Top = desctop.Bottom - this.Height;
+            BtnShow.Visibility = Visibility.Visible;
+        }
+
+
+        private void BtnShow_MouseEnter(object sender, MouseEventArgs e)
+        {
+            var desctop = System.Windows.SystemParameters.WorkArea;
+            this.Left = desctop.Right - (this.Width);
+            this.Top = desctop.Bottom - (this.Height);
+            BtnShow.Visibility = Visibility.Hidden;
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            List2.Height = 220;
+            List2.Visibility = Visibility.Visible;
+            if (List1.Visibility == Visibility.Visible)
+            {
+                List1.Height = 0;
+                List1.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            List1.Height = 220;
+            List1.Visibility = Visibility.Visible;
+            if (List2.Visibility == Visibility.Visible)
+            {
+                List2.Height = 0;
+                List2.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
